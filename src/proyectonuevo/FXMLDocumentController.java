01bc3922a9ca44000265557df8abd419391e46b2
Miguel <Miguel@LAPTOP-UFI19A04>
2020-10-30 12:33:45 +0100

    Segundo commit:   - Hemos añadido una barra de progreso

--- src/proyectonuevo/FXMLDocumentController.java
+++ src/proyectonuevo/FXMLDocumentController.java
@@ -23,7 +23,6 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
         label.setText("Hello World!");
     }
     
